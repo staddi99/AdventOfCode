a198f23168f08e055e5f03bb8c8c460543516e49
Maximilian Stadtmüller <maximilian.stadtmueller@staddi99.com>
2021-12-12 20:29:04 +0100

    ✨ 2021 Day 11

--- 2021/README.md
+++ 2021/README.md
@@ -24,7 +24,7 @@ Collect stars by solving puzzles. Two puzzles will be made available on each day
 *  [✅ Day 8: Seven Segment Search](day_8/)
 *  [✅ Day 9: Smoke Basin](day_9/)
 *  [✅ Day 10: Syntax Scoring](day_10/)
-*  [❔ Day 11: TBD]()
+*  [✅ Day 11: Dumbo Octopus](day_11/)
 *  [❔ Day 12: TBD]()
 *  [❔ Day 13: TBD]()
 *  [❔ Day 14: TBD]()
---
+++ 2021/day_11/code.js
@@ -0,0 +1,65 @@
+import input from './input.js';
+
+const inputArray = () => {
+  return input.split('\n').map((line) => line.split('').map(Number));
+};
+
+const increaseCellValue = (input, row, column, flashed = new Set()) => {
+  if (row < 0 || row >= input.length) return;
+  if (column < 0 || column >= input[0].length) return;
+  if (flashed.has(`${row}${column}`)) return;
+
+  const value = input[row][column];
+  const newValue = value + 1;
+  input[row][column] = newValue;
+  if (newValue <= 9) {
+    return;
+  }
+
+  flashed.add(`${row}${column}`);
+  input[row][column] = 0;
+  increaseCellValue(input, row - 1, column - 1, flashed);
+  increaseCellValue(input, row - 1, column, flashed);
+  increaseCellValue(input, row - 1, column + 1, flashed);
+  increaseCellValue(input, row, column - 1, flashed);
+  increaseCellValue(input, row, column + 1, flashed);
+  increaseCellValue(input, row + 1, column - 1, flashed);
+  increaseCellValue(input, row + 1, column, flashed);
+  increaseCellValue(input, row + 1, column + 1, flashed);
+};
+
+const iterate = (input) => {
+  const flashed = new Set();
+  for (let r = 0; r < input.length; r++) {
+    const row = input[r];
+    for (let c = 0; c < row.length; c++) {
+      increaseCellValue(input, r, c, flashed);
+    }
+  }
+  return flashed.size;
+};
+
+function partOne() {
+  let input = inputArray();
+  let totalFlashed = 0;
+  for (let i = 0; i < 100; i++) {
+    totalFlashed += iterate(input);
+  }
+  return totalFlashed;
+}
+
+function partTwo() {
+  let input = inputArray();
+  let step = 1;
+  while (true) {
+    const flashedCount = iterate(input);
+    if (flashedCount === 100) {
+      break;
+    }
+    step++;
+  }
+  return step;
+}
+
+console.log('Part 1: ' + partOne());
+console.log('Part 2: ' + partTwo());
