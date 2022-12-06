5b36048861bb36dfaac371c847e35b6be2e77d70
Maximilian Stadtmüller <maximilian.stadtmueller@staddi99.com>
2022-12-06 19:50:42 +0100

    ✨ 2022 Day 2

---
+++ 2022/day_2/code.js
@@ -0,0 +1,51 @@
+import input from './input.js';
+
+const inputArray = input.split('\n');
+
+const scores = {
+  A: 1,
+  B: 2,
+  C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+function partOne() {
+  var sum = 0;
+  inputArray.forEach((element) => {
+    const [a, b] = element.split(' ');
+    sum += scores[b];
+    sum +=
+      scores[a] - scores[b] == 0
+        ? 3
+        : scores[a] - scores[b] == 1
+        ? 0
+        : scores[a] - scores[b] + 3 == 1
+        ? 0
+        : 6;
+  });
+  return sum;
+}
+
+function partTwo() {
+  var sum = 0;
+  inputArray.forEach((element) => {
+    const [a, b] = element.split(' ');
+    switch (b) {
+      case 'X':
+        sum += scores[a] - 1 > 0 ? scores[a] - 1 : scores[a] + 2;
+        break;
+      case 'Y':
+        sum += scores[a] + 3;
+        break;
+      case 'Z':
+        sum += (scores[a] + 1 > 3 ? scores[a] - 2 : scores[a] + 1) + 6;
+        break;
+    }
+  });
+  return sum;
+}
+
+console.log('Part 1: ' + partOne());
+console.log('Part 2: ' + partTwo());
